dd4619eb22a59ec877b11a78afaaa2146b198929
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-02-04 17:51:35 +0100

    feat(core): add custom errors

---
+++ packages/vs3/src/core/error/error.ts
@@ -0,0 +1,47 @@
+import z from "zod";
+import { StorageErrorCode } from "./codes";
+
+const errorSchema = z.object({
+	origin: z.enum(["client", "server"]),
+	message: z.string(),
+	code: z.enum(Object.values(StorageErrorCode)),
+	details: z.unknown(),
+});
+
+const clientErrorSchema = errorSchema.omit({
+	origin: true,
+});
+
+const serverErrorSchema = errorSchema.omit({
+	origin: true,
+});
+
+export class StorageError extends Error {
+	readonly code: StorageErrorCode;
+	readonly details: unknown;
+
+	constructor(error: z.infer<typeof errorSchema>) {
+		super(error.message);
+		this.name = "sdfsdf";
+		this.code = error.code;
+		this.details = error.details;
+	}
+}
+
+export class StorageClientError extends StorageError {
+	constructor(error: z.infer<typeof clientErrorSchema>) {
+		super({
+			origin: "client" as const,
+			...error,
+		});
+	}
+}
+
+export class StorageServerError extends StorageError {
+	constructor(error: z.infer<typeof serverErrorSchema>) {
+		super({
+			origin: "server" as const,
+			...error,
+		});
+	}
+}
