6b91c9cb3f623b01c239f67ca2aa190460e5bc94
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-02-10 16:44:38 +0100

    feat(docs): implement docs site nav

---
+++ apps/docs/components/site-nav.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import type React from "react";
+import type { JSX } from "react";
+import { cn } from "@/lib/utils";
+
+export function SiteNav({
+	className,
+	items,
+	...props
+}: React.ComponentProps<"nav"> & {
+	items: { label: string; href: string; icon?: JSX.Element }[];
+}) {
+	return (
+		<nav
+			className={cn(
+				"mx-auto flex h-(--header-height) w-full max-w-7xl items-center justify-start px-8 py-6",
+				className,
+			)}
+			data-slot="site-nav"
+			{...props}
+		>
+			<div className="ml-auto flex items-center justify-center gap-8 [&_svg]:size-4 [&_svg]:fill-zinc-600 [&_svg]:group-hover/nav-item:fill-violet-600">
+				{items.map(({ href, icon: Icon, label }) => (
+					<Link
+						className="group/nav-item flex items-center justify-center gap-2 font-medium text-sm text-zinc-600 transition-colors hover:text-violet-600"
+						href={href}
+						key={href}
+					>
+						{Icon && Icon}
+						{label}
+					</Link>
+				))}
+			</div>
+		</nav>
+	);
+}
