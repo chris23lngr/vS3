320335fb807b5bd4f705efa4a14e48eb4926ebbc
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-02-10 16:50:12 +0100

    feat(docs): udpate mdx components

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -2,6 +2,10 @@ import * as Steps from "fumadocs-ui/components/steps";
 import * as Tabs from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import Link from "next/link";
+import { Callout } from "./components/callout";
+import { CodeTabs, CodeTabsContent } from "./components/code-tabs";
+import { cn } from "./lib/utils";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
@@ -11,3 +15,102 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
 		...components,
 	};
 }
+
+export const components = {
+	// ...defaultMdxComponents,
+	...Steps,
+	...Tabs,
+	// ...Callout,
+	h1: ({ children }: { children: React.ReactNode }) => (
+		<h1 className="font-bold text-9xl text-red-600">{children}</h1>
+	),
+	h2: ({ children }: { children: React.ReactNode }) => (
+		<h2 className="mt-12 mb-2 font-semibold text-2xl text-zinc-800">
+			{children}
+		</h2>
+	),
+	h3: ({ children }: { children: React.ReactNode }) => (
+		<h3 className="mt-8 mb-4 font-semibold text-lg">{children}</h3>
+	),
+	h4: ({ children }: { children: React.ReactNode }) => (
+		<h4 className="mt-10 mb-4 font-semibold text-base">{children}</h4>
+	),
+	h5: ({ children }: { children: React.ReactNode }) => (
+		<h5 className="font-bold text-sm">{children}</h5>
+	),
+	h6: ({ children }: { children: React.ReactNode }) => (
+		<h6 className="font-bold text-xs">{children}</h6>
+	),
+	strong: ({ children }: { children: React.ReactNode }) => (
+		<strong className="font-medium text-zinc-800">{children}</strong>
+	),
+	p: ({ children }: { children: React.ReactNode }) => (
+		<p className="mb-4 text-[0.9375rem] text-zinc-600 leading-relaxed">
+			{children}
+		</p>
+	),
+	ul: ({ children }: { children: React.ReactNode }) => (
+		<ul className="mt-4 mb-6 list-outside list-disc space-y-2 pl-3.5 text-[0.9375rem] text-body text-zinc-600 leading-7 marker:text-zinc-300 [&>li]:pl-2">
+			{children}
+		</ul>
+	),
+	ol: ({ children }: { children: React.ReactNode }) => (
+		<ol className="mb-4 list-inside list-decimal text-[0.9375rem] text-zinc-600">
+			{children}
+		</ol>
+	),
+	a: ({ children, href }: { children: React.ReactNode; href: string }) => (
+		<Link
+			className="rounded-sm font-medium text-zinc-800 underline underline-offset-4 transition-colors hover:text-violet-600 focus-visible:text-violet-500 focus-visible:decoration-0 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-violet-500 focus-visible:ring-offset-2"
+			href={href}
+		>
+			{children}
+		</Link>
+	),
+	pre: ({
+		className,
+		children,
+		title,
+		...props
+	}: React.ComponentProps<"pre">) => {
+		return (
+			<div className="mb-4 overflow-hidden rounded-xl bg-zinc-800 p-1">
+				{title && (
+					<div className="flex items-center justify-start gap-4 px-2 py-2">
+						<p className="font-medium text-xs text-zinc-400">{title}</p>
+					</div>
+				)}
+				<pre
+					className={cn(
+						"scrollbar-dark overflow-x-auto border border-zinc-900",
+						"inset-shadow-2xs inset-shadow-white/15 rounded-lg bg-zinc-700 py-4 text-sm",
+						"sm:[&>code]:[counter-reset:line] [&_.line]:before:mr-0 [&_.line]:before:w-0 [&_.line]:before:content-none sm:[&_.line]:before:mr-6 sm:[&_.line]:before:inline-block sm:[&_.line]:before:w-4 sm:[&_.line]:before:text-right sm:[&_.line]:before:text-xs sm:[&_.line]:before:text-zinc-400 sm:[&_.line]:[counter-increment:line] sm:[&_.line]:before:[content:counter(line)]",
+						className,
+					)}
+					{...props}
+				>
+					{children}
+				</pre>
+			</div>
+		);
+	},
+	code: ({ className, ...props }: React.ComponentProps<"code">) => {
+		if (typeof props.children === "string") {
+			return (
+				<code
+					className={cn(
+						"mx-0.5 rounded-md border border-zinc-200 bg-background px-1.5 py-0.5 font-mono text-sm shadow-xs",
+					)}
+					{...props}
+				>
+					{props.children}
+				</code>
+			);
+		}
+		// return children
+		return <code className={cn(className)} {...props} />;
+	},
+	Callout,
+	CodeTabs,
+	CodeTabsContent,
+};
